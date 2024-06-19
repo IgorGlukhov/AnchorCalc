03666bfac605dc7cf5b3979dad153d96e4bc9d07
Igor Gluhov <Huhovigor@yandex.ru>
2024-06-19 11:33:36 +0800

    Introduce Bootstrapper

---
+++ AnchorCalc.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using AnchorCalc.Views;
+
+namespace AnchorCalc.Bootstrapper
+{
+    public class Bootstrapper:IDisposable
+    {
+        public Window Run()
+        {
+            var mainWindow=new MainWindow();
+            mainWindow.Show();
+            return mainWindow;
+        }
+        public void Dispose()
+        {            
+        }
+    }
+}
